292869dd538ac4bac50d2eba2bce953703ec10d3
Akhlaq9897 <maazrahman24@gmail.com>
2025-08-07 22:07:24 +0530

    load_preprocess.py

---
+++ notebooks/load_preprocess.py
@@ -0,0 +1,13 @@
+from sklearn.datasets import load_iris
+import pandas as pd
+import os
+
+# Load Iris dataset
+iris = load_iris(as_frame=True)
+df = iris.frame
+
+# Save to CSV in data/ folder
+os.makedirs("data", exist_ok=True)
+df.to_csv("data/iris.csv", index=False)
+
+print("Saved preprocessed dataset to data/iris.csv")
