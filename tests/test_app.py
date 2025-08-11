9a01ce0ed776cf3256e0bd83bc856032c12ae9e9
Akhlaq9897 <maazrahman24@gmail.com>
2025-08-11 19:54:26 +0530

    Delete tests/test_app.py

--- tests/test_app.py
+++
@@ -1,9 +0,0 @@
-from fastapi.testclient import TestClient
-from app.app import app
-
-client = TestClient(app)
-
-def test_home():
-    response = client.get("/")
-    assert response.status_code == 200
-    assert "message" in response.json()
